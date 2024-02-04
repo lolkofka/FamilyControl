33856c526f9633de78a7f3f30af349f01b538985
lolkof <lolkofprog@gmail.com>
2024-02-04 03:25:11 +0300

    added restart policy docker

--- rebuild.sh
+++ rebuild.sh
@@ -2,5 +2,5 @@ docker container stop famcontrol
 docker container rm famcontrol
 docker image rm famcontroli
 docker build -t famcontroli .
-docker run -d --name famcontrol famcontroli
+docker run -d --name famcontrol --restart always famcontroli
 docker container ls
