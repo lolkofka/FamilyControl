71c4f6af8b143f4b119e1020a58758b9613c72aa
lolkof <lolkofprog@gmail.com>
2024-02-07 23:18:28 +0300

    added restart policy docker

--- additions/control.py
+++ additions/control.py
@@ -11,8 +11,10 @@ pc.get_device(config.deviceName)
 
 def needOffPc():
     workTime = database.get('workTime')
+    print(workTime, config.timeToPlay)
     if not workTime: return False
     if workTime > config.timeToPlay: return True
+    else: return False
 
 def startControl(): #Основной цикл
     resetDay = database.get('resetDay')
@@ -22,15 +24,18 @@ def startControl(): #Основной цикл
             database.set('resetDay', (time.time()+3600*3)//86400) 
             
     while True: # Запуск бесконечного цикла
-        pcStatus = pc.get_status()
-        if not pcStatus or needOffPc(): # Проверка на включение компьютера в алисе
-            pc.set_state(False) # Запрос на выключение
-        else:
-            workTime = database.get('workTime')
-            if not workTime: workTime = 0
-            database.set('workTime', workTime+5) # Добавляем 5 секунд к времени
-        hours = time.strftime("%H:%M", time.gmtime((time.time()+3600*3)))
-        if hours == config.resetTime:
-            database.set('workTime', 0)
-            database.set('resetDay', (time.time()+3600*3)//86400) #Сброс времени ночью
-        time.sleep(5) #Ожидание 5 секунд
+        try:
+            pcStatus = pc.get_status()
+            if not pcStatus or needOffPc(): # Проверка на включение компьютера в алисе
+                pc.set_state(False) # Запрос на выключение
+            else:
+                workTime = database.get('workTime')
+                if not workTime: workTime = 0
+                database.set('workTime', workTime+5) # Добавляем 5 секунд к времени
+            hours = time.strftime("%H:%M", time.gmtime((time.time()+3600*3)))
+            if hours == config.resetTime:
+                database.set('workTime', 0)
+                database.set('resetDay', (time.time()+3600*3)//86400) #Сброс времени ночью
+            time.sleep(5) #Ожидание 5 секунд
+        except Exception as e:
+            print(e)
